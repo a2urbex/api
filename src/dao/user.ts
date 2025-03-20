5dca2b32247d2712826331730a305dc39b2e4347
Clement Po <clement.pommerie@gmail.com>
2025-03-20 19:21:02 +0100

    Update user.ts

--- src/dao/user.ts
+++ src/dao/user.ts
@@ -35,7 +35,7 @@ const user = {
   },
 
   getAll: () => {
-    const sql = `SELECT id, email, username, roles FROM user`
+    const sql = `SELECT id, image, username, roles FROM user`
     return db.query(sql)
   },
 }
